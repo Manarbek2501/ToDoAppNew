75d4883bb67e4a99306925727ff56d513150e988
AImangazin <aimangazin@dar.io>
2025-05-29 18:24:22 +0500

    Initial Commit

---
+++ ToDoAppNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoAppNew
+//
+//  Created by Manarbek Bibit on 29.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ToDoAppNewTests/ToDoAppNewTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppNewTests.swift
+//  ToDoAppNewTests
+//
+//  Created by Manarbek Bibit on 29.05.2025.
+//
+
+import Testing
+@testable import ToDoAppNew
+
+struct ToDoAppNewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
